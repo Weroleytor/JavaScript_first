dd3b96dd4d8c74a6bdbc04b8d47f5b06ad98cb74
Ulises Heraldez <ulises_heraldez@hotmail.com>
2021-09-05 22:09:54 -0600

    test

--- main.ts
+++ main.ts
@@ -1,71 +1,71 @@
-//1.- Saludar
-function saludar(nombre){
-    return "shola, " + nombre;
-}
+// //1.- Saludar
+// function saludar(nombre){
+//     return "shola, " + nombre;
+// }
 
-console.log(saludar("Ulises Heraldez"));
-
-
-//2.- Tipos, Boolean
-let esVerdadero = true;
-console.log(esVerdadero);
-
-let futbolista: [string, string];
-futbolista = ['Messi', '34'];
-
-console.log(`El nombre es ${futbolista[0]}`);
-console.log(`La edad es ${futbolista[1]}`);
+// console.log(saludar("Ulises Heraldez"));
 
 
-//3.- Tipos, Number
-let entero: number = 6;
-let hexadecimal: number = 0xf00d;
-let binario: number = 0b1010;
-let octal: number = 0o744;
+// //2.- Tipos, Boolean
+// let esVerdadero = true;
+// console.log(esVerdadero);
+
+// let futbolista: [string, number];
+// futbolista = ['Messi', 34];
+
+// console.log(`El nombre es ${futbolista[0]}`);
+// console.log(`La edad es ${futbolista[1]}`);
 
 
-//4.- Tipos, String
-let marca: string = 'Toyota';
-let modelo: string = "Tacoma";
+// //3.- Tipos, Number
+// let entero: number = 6;
+// let hexadecimal: number = 0xf00d;
+// let binario: number = 0b1010;
+// let octal: number = 0o744;
 
 
-//uso del caracter backtick
-let nombre: string = "Raul";
-let apellido: string = "Jimenez";
-let impresion: string = `
-Nombre: ${nombre}
-Apellido: ${apellido}
-`;
+// //4.- Tipos, String
+// let marca: string = 'Toyota';
+// let modelo: string = "Tacoma";
 
-enum MarcasDeAutos {
-    Toyota = 100,
-    Chevrolet,
-    Ford
-}
 
-let tacoma: MarcasDeAutos = MarcasDeAutos.Toyota;
-console.log(tacoma);
+// //uso del caracter backtick
+// let nombre: string = "Raul";
+// let apellido: string = "Jimenez";
+// let impresion: string = `
+// Nombre: ${nombre}
+// Apellido: ${apellido}
+// `;
 
-let stringOne = MarcasDeAutos[1];
-let stringTwo = MarcasDeAutos[MarcasDeAutos.Chevrolet];
+// enum MarcasDeAutos {
+//     Toyota = 100,
+//     Chevrolet,
+//     Ford
+// }
 
-console.log(MarcasDeAutos[0])
-console.log(MarcasDeAutos[1])
+// let tacoma: MarcasDeAutos = MarcasDeAutos.Toyota;
+// console.log(tacoma);
 
-console.log(stringOne);
-console.log(stringTwo);
+// let stringOne = MarcasDeAutos[1];
+// let stringTwo = MarcasDeAutos[MarcasDeAutos.Chevrolet];
 
-function imprimirId(id: number | string | undefined) {
-    console.log(`El id es ${id}`);
-}
+// console.log(MarcasDeAutos[0])
+// console.log(MarcasDeAutos[1])
 
-imprimirId(1);
-imprimirId('abc');
-imprimirId(undefined);
+// console.log(stringOne);
+// console.log(stringTwo);
 
-function imprimirEtiqueta(etiqueta: {label: string}) {
-    console.log(etiqueta.label);
-}
+// function imprimirId(id: number | string | undefined) {
+//     console.log(`El id es ${id}`);
+// }
 
-let miEtiqueta = { numero: 10, label: "Esta es mi etiqueta"};
-imprimirEtiqueta(miEtiqueta);
+// imprimirId(1);
+// imprimirId('abc');
+// imprimirId(undefined);
+
+// function imprimirEtiqueta(etiqueta: {label: string}) {
+//     console.log(etiqueta.label);
+// }
+
+// let miEtiqueta = { numero: 10, label: "Esta es mi etiqueta"};
+// imprimirEtiqueta(miEtiqueta);
